3510edeff21c187d5b6add171f7de0e80564bcd6
Masud_Rana <masudranaorg71@gmail.com>
2025-01-25 22:28:04 +0600

    More From Recent A,B

---
+++ A_Minimal_Coprime.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+//typedef ...
+#define ll long long
+#define endl        '\n'
+#define en          '\n';
+const ll mod = 1e9+7;
+
+void solve() {
+    int l,r;    cin>>l>>r;
+    if(l==1 && (r==1)){
+        cout<<"1\n";
+        return;
+    } 
+    cout<<r-l<<en 
+
+}
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int t=1;
+    cin >> t;
+    while(t--)
+        solve();
+    return 0;
+}
