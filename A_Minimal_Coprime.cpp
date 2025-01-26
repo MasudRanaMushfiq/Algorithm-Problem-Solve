c5b8fdb165cc4e1834731fd536b439611a204ad1
Masud_Rana <masudranaorg71@gmail.com>
2025-01-26 07:56:50 +0600

    Last commit before exam

--- A_Minimal_Coprime.cpp
+++ A_Minimal_Coprime.cpp
@@ -1,26 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-//typedef ...
-#define ll long long
-#define endl        '\n'
-#define en          '\n';
-const ll mod = 1e9+7;
 
-void solve() {
-    int l,r;    cin>>l>>r;
-    if(l==1 && (r==1)){
-        cout<<"1\n";
+void solve(){
+    int l,r;
+        cin>>l>>r;
+    if(l==1 && r==1){
+        cout<<"1"<<endl;
         return;
-    } 
-    cout<<r-l<<en 
-
+    }
+    cout<<r-l<<endl;
 }
 
 int32_t main(){
-    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    int t=1;
-    cin >> t;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    
+    int t;
+    cin>>t;
     while(t--)
         solve();
-    return 0;
 }
